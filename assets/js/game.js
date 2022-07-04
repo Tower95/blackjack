13f10150107f4cbcc095d8a000fd4ebe02c892ad
Tower95 <torrestmi3@gmail.com>
2022-07-03 22:42:40 -0700

    optimizations and module

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,169 +1,200 @@
-'use strict'
-/**
- * C - Clubs
- * D - Dimond
- * H - Heart
- * S - Spades
- */
-
-let deck = [];
-const suits = ['C', 'D', 'H', 'S'];
-const figures = ['A', 'J', 'Q', 'K'];
-
-//html reference
-const buttomDraw = document.querySelector('#btnDraw');
-const buttomHalt = document.querySelector('#btnHalt');
-const buttomNew = document.querySelector('#btnNew');
-
-const points = document.querySelectorAll('span');
-const divPlayerCards = document.querySelector('#player-cards');
-const divCpuCards = document.querySelector('#cpu-cards');
-
-//points
-let playerPoints = 0,
-  cpuPoints = 0;
+//use model pattern
+const miModulo = (() => {
+  'use strict'
 
 
-const createDeck = (arr) => {
-  for (let i = 2; i <= 10; i++) {
+  let deck = [];
+
+  //html reference
+  const buttomDraw = document.querySelector('#btnDraw'),
+    buttomHalt = document.querySelector('#btnHalt'),
+    buttomNew = document.querySelector('#btnNew');
+
+  //html div cards sections
+  const points = document.querySelectorAll('span'),
+    divCardsPlayers = document.querySelectorAll('.divCard');
+
+
+  //points
+  let playersPoints = [];
+
+  const generateRandom = (base) => Math.floor(Math.random() * base)
+
+  const shuffle = (array) => {
+    let currendId = array.length
+
+    while (currendId != 0) {
+
+      //generate random
+      let randomId = generateRandom(currendId);
+      currendId--;
+
+      //swap
+      let temporal = array[currendId];
+      array[currendId] = array[randomId];
+      array[randomId] = temporal;
+    }
+
+    return array;
+  }
+
+  // this function start the new deck
+  const startGame = (numPlayers = 2) => {
+    deck = createDeck()
+    playersPoints = [];
+    for (let i = 0; i < numPlayers; i++) {
+      playersPoints.push(0);
+    }
+    points.forEach(point => point.innerHTML = 0);
+    divCardsPlayers.forEach(div => div.innerHTML = "");
+
+    buttomHalt.disabled = false;
+    buttomDraw.disabled = false;
+
+    console.log({ playersPoints });
+  }
+
+  const createDeck = () => {
+
+    const suits = ['C', 'D', 'H', 'S'],
+      figures = ['A', 'J', 'Q', 'K'];
+
+
+    for (let i = 2; i <= 10; i++) {
+      for (const suit of suits) {
+        deck.push(i + suit);
+      }
+    }
+
     for (const suit of suits) {
-      deck.push(i + suit);
+      for (const figure of figures) {
+        deck.push(figure + suit);
+      }
     }
+
+    return shuffle(deck);
+
   }
-  for (const suit of suits) {
-    for (const figure of figures) {
-      deck.push(figure + suit);
+
+  //draw
+  const drawOne = (array) => {
+    let max = array.length
+
+    if (max === 0) {
+      throw 'not card left in deck';
     }
+
+    let randomCard = generateRandom(max);
+    let card = array[randomCard];
+
+    array = array.splice(randomCard, 1);
+
+    return card;
   }
 
-}
-
-const generateRandom = (base) => Math.floor(Math.random() * base)
-
-const shuffle = (array) => {
-  let currendId = array.length
-
-  while (currendId != 0) {
-
-    //generate random
-    let randomId = generateRandom(currendId);
-    currendId--;
-
-    //swap
-    let temporal = array[currendId];
-    array[currendId] = array[randomId];
-    array[randomId] = temporal;
+  const cardValue = (card) => {
+    let value = card.substring(0, card.length - 1);
+    return (isNaN(value)) ?
+      (value === 'A') ? 11 : 10
+      : (value * 1);
   }
 
-}
-
-//draw
-const drawOne = (array) => {
-  let max = array.length
-
-  if (max === 0) {
-    throw 'not card left in deck';
+  // turn: 0 = first player, last turn it's the cpu
+  const addPoint = (card, turn) => {
+    playersPoints[turn] += cardValue(card);
+    points[turn].innerText = playersPoints[turn];
+    return playersPoints[turn];
   }
 
-  let randomCard = generateRandom(max);
-  let card = array[randomCard];
+  const renderCard = (card, turn) => {
 
-  array = array.splice(randomCard, 1);
-
-  return card;
-}
-
-const cardValue = (card) => {
-  let value = card.substring(0, card.length - 1);
-  return (isNaN(value)) ?
-    (value === 'A') ? 11 : 10
-    : (value * 1);
-}
-// cpu turn
-const cpuTurn = (pointsToWin) => {
-
-  do {
-    //calculate new value
-    let card = drawOne(deck);
-    cpuPoints += cardValue(card);
-
-    //generate and render card
-    points[1].innerText = cpuPoints;
     let newCard = document.createElement('img');
     newCard.src = `assets/cards/${card}.png`;
     newCard.classList.add('my-card');
-    divCpuCards.append(newCard)
+    divCardsPlayers[turn].append(newCard)
 
-    if (pointsToWin > 21) {
-      break;
-    }
-
-
-  } while ((cpuPoints < pointsToWin) && (pointsToWin <= 21));
-
-  setTimeout(() => {
-
-    let message = 'YOU WON!';
-    if (playerPoints > 21 && cpuPoints < 21) {
-      message = 'CPU WON!';
-    } else if ((playerPoints < 21) && (cpuPoints < playerPoints)) {
-      message = 'CPU WON';
-    } else if (playerPoints == cpuPoints) {
-      message = 'TIED';
-    } else if (playerPoints < cpuPoints && cpuPoints <= 21) {
-      message = 'CPU WON';
-    }
-    alert(message);
-  }, 1500);
-}
-
-createDeck();
-shuffle(deck);
-
-//draw event
-buttomDraw.addEventListener('click', () => {
-  //calculate new value
-  let card = drawOne(deck);
-  playerPoints += cardValue(card);
-
-  //generate and render card
-  points[0].innerText = playerPoints;
-  let newCard = document.createElement('img');
-  newCard.src = `assets/cards/${card}.png`;
-  newCard.classList.add('my-card');
-  divPlayerCards.append(newCard)
-
-  if (playerPoints > 21) {
-    console.warn('You Lose');
-    buttomDraw.disabled = true;
-    buttomDraw.disabled = true;
-    cpuTurn(playerPoints);
-
-  } else if (playerPoints === 21) {
-    console.warn('21, Great!');
-    buttomDraw.disabled = true;
-    cpuTurn(playerPoints);
   }
-});
+  const getWinner = () => {
 
-//Halt event
-buttomHalt.addEventListener('click', () => {
-  buttomHalt.disabled = true;
-  buttomDraw.disabled = true;
-  cpuTurn(playerPoints);
-});
+    const [minPoints, cpuPoints] = playersPoints;
+    setTimeout(() => {
 
-buttomNew.addEventListener('click', () => {
-  console.clear();
-  deck = [];
-  createDeck()
-  shuffle(deck);
-  buttomDraw.disabled = false;
-  buttomHalt.disabled = false;
-  divPlayerCards.innerHTML = "";
-  divCpuCards.innerHTML = "";
-  cpuPoints = 0;
-  playerPoints = 0;
-  points[1].innerText = playerPoints;
-  points[0].innerText = cpuPoints;
-});
+      let message = 'YOU WON!';
+      if (minPoints > 21 && cpuPoints < 21) {
+        message = 'CPU WON!';
+      } else if ((minPoints < 21) && (cpuPoints < minPoints)) {
+        message = 'CPU WON';
+      } else if (minPoints == cpuPoints) {
+        message = 'TIED';
+      } else if (minPoints < cpuPoints && cpuPoints <= 21) {
+        message = 'CPU WON';
+      }
+      alert(message);
+    }, 1500);
+  }
+
+  // cpu turn
+  const cpuTurn = (pointsToWin) => {
+
+    let cpuPoints = 0;
+    do {
+      //calculate new value
+      let card = drawOne(deck);
+
+      //generate and render card
+      cpuPoints = addPoint(card, playersPoints.length - 1);
+
+      renderCard(card, playersPoints.length - 1);
+
+      if (pointsToWin > 21) {
+        break;
+      }
+
+
+    } while ((cpuPoints < pointsToWin) && (pointsToWin <= 21));
+
+    getWinner();
+  }
+
+
+  //draw event
+  buttomDraw.addEventListener('click', () => {
+    //calculate new value
+    let card = drawOne(deck);
+    //generate and render card
+    let playerPoints = addPoint(card, 0);
+
+    renderCard(card, 0);
+
+    if (playerPoints > 21) {
+      console.warn('You Lose');
+      buttomDraw.disabled = true;
+      buttomDraw.disabled = true;
+      cpuTurn(playerPoints);
+
+    } else if (playerPoints === 21) {
+      console.warn('21, Great!');
+      buttomDraw.disabled = true;
+      cpuTurn(playerPoints);
+    }
+  });
+
+  //Halt event
+  buttomHalt.addEventListener('click', () => {
+    buttomHalt.disabled = true;
+    buttomDraw.disabled = true;
+    cpuTurn(playersPoints[0]);
+  });
+
+  // buttomNew.addEventListener('click', () => {
+  //   console.clear();
+
+  //   startGame();
+
+  // });
+
+  return {
+    newGame : startGame 
+  };
+
+})();
